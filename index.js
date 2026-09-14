b0568ea1252560c0ca898017d9d7d62375046328
georgevicbell <georgevicbell@users.noreply.github.com>
2026-09-14 23:17:23 +0000

    deploy: 0343419af3ab17ca0127ded268619b92e51d8085

--- index.js
+++ index.js
@@ -404,6 +404,14 @@ var vite_config_default = defineConfig({
       strict: true,
       deny: ["**/.*"]
     }
+  },
+  test: {
+    coverage: {
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ["text", "json-summary", "json"],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true
+    }
   }
 });
 
